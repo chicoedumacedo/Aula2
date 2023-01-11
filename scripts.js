e52da35a2604a7cde1e2e9fdbad907b00af4c420
Eduardo Macedo <chicoedumacedo@gmail.com>
2023-01-11 10:01:17 -0300

    criar calculadora, aula sobre variaveis e constantes

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// //document.getElementById("conteiner").innerHTML = "Conteudo";
+
+// let container = document.getElementById("container");
+
+// // let macas = 2;
+// // let banana = 3;
+// // let soma = macas + banana; //o mais e um operador aritmético
+
+// // container.innerHTML = soma;
+
+// // let texto = "Meu nome é";
+// // let nome = "Eduardo Rocha";
+
+// // let soma = texto + nome; // o mais é um sinal de concatenação
+
+// // container.innerHTML = soma
+
+// // const NASCIMENTO = "09/07/1981";
+// // let nome = "Eduardo Rocha";
+// // var nome = "Macedo";
+// // var nome = "Francisco";
+
+// container.innerHTML = nome + NASCIMENTO;
+
+// NASCIMENTO = 10/12/2022 //não muda mesmo que atribua outro valor pra const.
+
+
+// // container.innerHTML = "uma coisa";
+// // container.append("outra coisa");
+
+function somar()
+{
+    let v1 = Number(valor1.value);
+    let v2 = Number(valor2.value);
+    document.getElementById("resultado").innerHTML = v1 + v2; 
+}
+
